31f9bba90c0bacca8767c34bd0a551b347cad4d6
Anton Loskutov <mrloskutov@live.ru>
2022-09-25 22:32:41 +0300

    Finish version

--- RgbSliders/ViewController.swift
+++ RgbSliders/ViewController.swift
@@ -11,20 +11,36 @@ class ViewController: UIViewController {
 
     @IBOutlet var colorView: UIView!
     
-    @IBOutlet var alphaRedOutlet: UILabel!
-    @IBOutlet var alphaGreenOutlet: UILabel!
-    @IBOutlet var alphaBlueOutlet: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
-    @IBOutlet var redSliderOutlet: UISlider!
-    @IBOutlet var greenSliderOutlet: UISlider!
-    @IBOutlet var blueSliderOutlet: UISlider!
-    
+    @IBOutlet var saturationRedLable: UILabel!
+    @IBOutlet var saturationGreenLable: UILabel!
+    @IBOutlet var saturationBlueLable: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        colorView.layer.cornerRadius = 15
+        setupSaturationLabel()
     }
-
-
+    
+    @IBAction func sliderAction() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        saturationRedLable.text = String(format: "%.2f", redSlider.value)
+        saturationGreenLable.text = String(format: "%.2f", greenSlider.value)
+        saturationBlueLable.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func setupSaturationLabel() {
+        saturationRedLable.text = redSlider.value.formatted()
+        saturationGreenLable.text = greenSlider.value.formatted()
+        saturationBlueLable.text = blueSlider.value.formatted()
+    }
 }
 
