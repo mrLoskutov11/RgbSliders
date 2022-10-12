1c8044d2524c5be897a54da56637b9f2828c266a
Anton Loskutov <a424056193@me.com>
2022-10-12 17:45:48 +0300

    Work on mistakes

--- RgbSliders/ViewController.swift
+++ RgbSliders/ViewController.swift
@@ -9,40 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var background: UIView!
     @IBOutlet var colorView: UIView!
-    
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
     
     @IBOutlet var saturationRedLable: UILabel!
     @IBOutlet var saturationGreenLable: UILabel!
     @IBOutlet var saturationBlueLable: UILabel!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         colorView.layer.cornerRadius = 15
-        setupSaturationLabel()
+        setColor()
+        setValue()
     }
     
-    @IBAction func sliderAction() {
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            saturationRedLable.text = string(from: sender)
+        case greenSlider:
+            saturationGreenLable.text = string(from: sender)
+        default:
+            saturationBlueLable.text = string(from: sender)
+        }
+    }
+    private func setColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
         )
-        saturationRedLable.text = String(format: "%.2f", redSlider.value)
-        saturationGreenLable.text = String(format: "%.2f", greenSlider.value)
-        saturationBlueLable.text = String(format: "%.2f", blueSlider.value)
     }
-    
-    private func setupSaturationLabel() {
-        saturationRedLable.text = redSlider.value.formatted()
-        saturationGreenLable.text = greenSlider.value.formatted()
-        saturationBlueLable.text = blueSlider.value.formatted()
+    private func setValue() {
+        saturationRedLable.text = string(from: redSlider)
+        saturationGreenLable.text = string(from: greenSlider)
+        saturationBlueLable.text = string(from: blueSlider)
+    }
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
-
